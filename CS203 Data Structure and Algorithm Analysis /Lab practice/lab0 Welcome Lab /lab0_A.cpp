ea7fedb030174978a338871c1fd354ee589c8ab7
lxriscute0501 <lixuanran798@gmail.com>
2025-02-22 20:06:40 +0800

    Search Problem I

---
+++ CS203 Data Structure and Algorithm Analysis /Lab practice/lab0 Welcome Lab /lab0_A.cpp
@@ -0,0 +1,27 @@
+//数据量小，双重循环查找即可
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int n, t;
+   int a[1001], b[1001];
+   bool bo;
+   
+   cin >> n;
+   for (int i=1; i<=n; i++)
+       cin >> a[i];
+   cin >> t;
+   for (int i=1; i<=t; i++)
+       cin >> b[i];
+   for (int i=1; i<=t; i++)
+   {
+       bo = false;
+       for (int j=1; j<=n; j++)
+           if (b[i] == a[j]) {
+               bo = true;
+               break;
+           }
+       if (bo) cout << "yes" << endl; else cout << "no" << endl;
+   }
+}
