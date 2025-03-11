278ddec3bc441655616bcb25f0d0b0d5825e7431
lxriscute0501 <lixuanran798@gmail.com>
2025-03-11 10:33:39 +0800

    Create A2_1.java

---
+++ CS109 Introduction to Computer(JavaA) Programming /Assignment /assignment 2/A2_1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n, m;
+        int[] a = new int[51];
+        int[] b = new int[51];
+
+        n = scanner.nextInt();
+        for (int i = 1; i <= n; i++)
+            a[i] = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++)
+            b[i] = scanner.nextInt();
+
+        m = scanner.nextInt();
+        int num = (a[m] == -1 ? 1 : 0) + (b[m] == -1 ? 1 : 0);
+        System.out.println(num);
+    }
+}
