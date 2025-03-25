5a0cff03914c8a4c538fec94e6f7ece052105f3a
lxriscute0501 <lixuanran798@gmail.com>
2025-03-25 15:08:36 +0800

    Update and rename Lab6E5.java to Lab5E5.java

--- CS109 Introduction to Computer(JavaA) Programming /Lab material/lab5/Lab5E5.java
+++ CS109 Introduction to Computer(JavaA) Programming /Lab material/lab5/Lab5E5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Lab6E5 {
+public class Lab5E5 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int T = in.nextInt();    //get the number of test cases
